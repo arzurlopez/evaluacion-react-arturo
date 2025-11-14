0db77d6377f302abb0807f47193c14e7b7c5266a
arzurlopez <arzurlopez@gmail.com>
2025-11-14 12:54:09 +0100

    CambiosCuatro

--- src/components/Empresas.jsx
+++ src/components/Empresas.jsx
@@ -1,45 +1,27 @@
 
 import { useEffect } from "react"
-import empresas from '../assets/empresas.json'
+import data from '../assets/empresas.json'
 
 
 export default function Empresas() {
 
   return (
     <>
-    Empresas
+    <ul>
+        {
+          data.map((item, index) => (
+          <li key={index}>
+            {item.nombre} {item.ciudad}
+            </li>
+        )
+          )
+        }
+      </ul>
+
+
     </>
   )
 }
 
 
 
-
-/* function VerRutasExternas() {
-  const [escapadas, setEscapadas] = useState([])
-  useEffect(() => {
-    const cargarEscapadas = async () => {
-      try {
-        const respuesta = await fetch('http://localhost:5173/escapadas.json')
-        const data = await respuesta.json()
-        setEscapadas(data)
-        console.log(data)
-      } catch (e) {
-        console.error(`Se ha producido un error ${e}`)
-      }
-    }
-    cargarEscapadas();
-  }, []) */
-
-
-
-/* 
-  import { useEffect } from "react"
-
-export default function Empresas() {
-  return (
-    <>
-    Empresas
-    </>
-  )
-} */
